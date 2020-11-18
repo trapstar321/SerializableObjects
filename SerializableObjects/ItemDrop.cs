e0fdc8afa41a3ffdce5495387b1874d93a57d955
Your Name <ciril.studenovic@gmail.com>
2020-11-18 20:07:36 +0100

    ItemDrop

---
+++ SerializableObjects/ItemDrop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializableObjects
+{
+    [Serializable]
+    public class ItemDrop
+    {
+        public Item item;
+        public int quantity;
+    }
+}
